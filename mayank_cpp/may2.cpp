f0196d85778e143d93e623a6374235e959f0fde1
Mayank <themayank1729@gmail.com>
2024-04-24 09:57:09 +0530

    two folders added

---
+++ mayank_cpp/may2.cpp
@@ -0,0 +1,6 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+       cout<<"mayank  2 is f***ing best";
+}
---
+++ mayank_cpp/may3.cpp
@@ -0,0 +1,6 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+       cout<<"mayank  3 is f***ing best";
+}
