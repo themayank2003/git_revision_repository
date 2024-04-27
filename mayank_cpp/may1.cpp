2da2b96b93c0a31cb7d0a081f3d8d45dbb70e83a
themayank2003 <themayank1729@gmail.com>
2024-04-27 16:09:27 +0530

    all folders are deleted

--- mayank_cpp/may1.cpp
+++
@@ -1,6 +0,0 @@
-#include<bits/stdc++.h>
-using namespace std;
-int main()
-{
-       cout<<"mayank is f***ing best";
-}
--- mayank_cpp/may4.cpp
+++
@@ -1,6 +0,0 @@
-#include<bits/stdc++.h>
-using namespace std;
-int main()
-{
-       cout<<"mayank  4 is f***ing best";
-}
